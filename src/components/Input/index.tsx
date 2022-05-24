6a932888ce3be2559c496862d3ef128a028db266
ivan ojiambo <ojiamboivan@gmail.com>
2022-05-24 06:57:33 +0300

    Created input component

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+
+export interface InputProps {
+  value: string;
+  name: string;
+  onChange?: (value: string) => void;
+}
+
+export const Input = ({ name, value, onChange }: InputProps) => {
+  const [inputValue, setInputValue] = useState(value);
+
+  useEffect(() => {
+    setInputValue(value);
+  }, [value]);
+
+  const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (typeof onChange === 'function') {
+      onChange(event.target.value);
+    } else {
+      setInputValue(event.target.value);
+    }
+  };
+
+  return <input name={name} value={inputValue} onChange={handleOnChange} />;
+};
+
+export default Input;
